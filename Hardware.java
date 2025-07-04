4fcb82a495161f0674d159f6fa7a337695d56329
Shamsheera  A T <96409324+shamsheerafarsheed@users.noreply.github.com>
2025-07-04 17:52:26 +0530

    Add files via upload

---
+++ Hardware.java
@@ -0,0 +1,36 @@
+package oopsDemo;
+
+public class Hardware {
+	private String name;
+	private String desc;
+	private int qty;
+	private double price;
+	//getter setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+
+}
