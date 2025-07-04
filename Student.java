390405c3608fff564b250f1ef5ebf2bdc20db11f
Shamsheera  A T <96409324+shamsheerafarsheed@users.noreply.github.com>
2025-07-04 18:29:15 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,34 +1,22 @@
-package constructorDemo;
+package ToStringDemo;
 
 public class Student {
 	int id;
 	String name;
-	int age;
-	//Creating parameterised Constructor
 	
-	Student(int id,String name,int age)
+	Student(int id,String name)
 	{
 		this.id=id;
 		this.name=name;
-		this.age=age;
 	}
-	void display()
+	//override toString()--->return object as String
+	public String toString()
 	{
-		System.out.println("ID:"+id+" "+" Name:"+name+" "+"Age:"+" "+age);
+		return "Student[ID=" +id+",Name=" +name+ "]";
 	}
-	
 	public static void main(String[] args) {
-		// creating an object and passing values
-		
-		Student s1=new Student(101,"Anusha",20);
-		Student s2=new Student(102,"Urmila",20);
-		Student s3=new Student(103,"Muskan",20);
-		Student s4=new Student(104,"Dhyan",20);
-		s1.display();
-		s2.display();
-		s3.display();
-		s4.display();
-		
-	}
+		Student s=new Student(1,"Kiran");
+		System.out.println(s);	
+		}
 
 }
